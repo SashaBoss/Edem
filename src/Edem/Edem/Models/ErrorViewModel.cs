a4441c1abc5aae562b09468b2a363666b719bf47
Aleksandr Maha <aleksandr.maha@anionsoft.com>
2020-06-29 09:38:43 +0300

    initial add MVC project

---
+++ src/Edem/Edem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Edem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
